a8e36092018acb7935a8fb84ccda5e6fc21f1a0b
robertotics4 <robertooliveiragp@gmail.com>
2022-06-09 16:02:01 -0300

    episode 01

---
+++ web/src/pages/app/index.tsx
@@ -0,0 +1,16 @@
+import { getSession, useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { GetServerSideProps } from "next";
+
+export default function Home() {
+  const { user } = useUser();
+
+  return (
+    <div>
+      <h1>Hello world</h1>
+
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+    </div>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = withPageAuthRequired();
